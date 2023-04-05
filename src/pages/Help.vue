1730227296188b37a75955dbd52ee453fcfc28f2
John Rhenier <john.sundiam@terp.no>
2023-04-05 16:08:31 +0800

    quasar project

---
+++ src/pages/Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias ipsa soluta molestiae, iste inventore saepe vitae non cumque. Sed cupiditate facilis culpa beatae quae neque eum iste placeat, est optio.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, delectus nulla voluptatibus nemo ullam necessitatibus aspernatur voluptatum magni harum dolor ipsa quasi officiis aut architecto, laborum rerum a voluptates autem.</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+})
+</script>
